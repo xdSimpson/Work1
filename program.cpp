db51d7cdd25dab12a5cd8b6657424b0bde8c286e
AlexBlack1338 <like4311@gmail.com>
2023-03-20 22:39:37 +0300

    Final result of the program

--- program.cpp
+++ program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "funcs.h"
 using namespace std;
 
 int main() {
@@ -8,25 +9,25 @@ int main() {
    cout << "Enter your choice:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n";
    cin >> choice;
    switch(choice) {
-      case 1:
-         cout << "Function Sum is in development" << endl;
-         break;
-      case 2:
-         cout << "Function Sub is in development" << endl;
-         break;
-      case 3:
-         cout << "Function Mul is in development" << endl;
-         break;
-      case 4:
-         if(b != 0) {
-            cout << "Function Div is in development" << endl;
-         }
-         else {
-            cout << "Error: Division by zero";
-         }
-         break;
-      default:
-         cout << "Invalid choice";
-   }
-   return 0;
+      case Sum:
+            result = sum(a, b);
+            break;
+        case Sub:
+            result = sub(a, b);
+            break;
+        case Mul:
+            result = mul(a, b);
+            break;
+        case Div:
+            result = div(a, b);
+            break;
+        default:
+            cout << "Error: wrong number of function\n";
+            return 1;
+    }
+
+    // Вывод результата
+    cout << "Результат: " << result << endl;
+
+    return 0;
 }
